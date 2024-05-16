cf8739d6140d178f16ea760a0325c65774eefcb7
Hyunju Lee <guswnee00@gmail.com>
2024-05-16 22:23:21 +0900

    Initial commit

---
+++ src/main/java/org/zerobase/hospitalappointmentproject/HospitalAppointmentProjectApplication.java
@@ -0,0 +1,13 @@
+package org.zerobase.hospitalappointmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAppointmentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalAppointmentProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerobase/hospitalappointmentproject/HospitalAppointmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerobase.hospitalappointmentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
