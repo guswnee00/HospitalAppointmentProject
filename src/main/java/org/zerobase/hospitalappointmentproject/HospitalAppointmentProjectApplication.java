676c3747d572f615e60bb70ca1504b53223eaf81
Hyunju Lee <guswnee00@gmail.com>
2024-05-19 17:55:17 +0900

    feat: @EnableJpaAuditing 추가

--- src/main/java/org/zerobase/hospitalappointmentproject/HospitalAppointmentProjectApplication.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/HospitalAppointmentProjectApplication.java
@@ -2,8 +2,10 @@ package org.zerobase.hospitalappointmentproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HospitalAppointmentProjectApplication {
 
     public static void main(String[] args) {
