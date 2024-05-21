80bfca4e70ec24003262da136ac0e0e05ab948a6
Hyunju Lee <guswnee00@gmail.com>
2024-05-21 21:35:19 +0900

    feat : Spring Security JWT

---
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/auth/security/JwtFilter.java
@@ -0,0 +1,66 @@
+package org.zerobase.hospitalappointmentproject.global.auth.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerobase.hospitalappointmentproject.global.auth.dto.CustomUserDetails;
+import org.zerobase.hospitalappointmentproject.global.auth.entity.UserEntity;
+
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+  private final JwtUtil jwtUtil;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                  FilterChain filterChain) throws ServletException, IOException
+  {
+
+    // request 에서 헤더 찾기
+    String authorization = request.getHeader("Authorization");
+    // 헤더 검증
+    if (authorization == null || !authorization.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      // 조건이 해당되면 종료
+      return;
+
+    }
+
+    // Bearer 제거
+    String token = authorization.split(" ")[1];
+    // 토큰 소멸 시간 검증
+    if (jwtUtil.isExpired(token)) {
+      filterChain.doFilter(request, response);
+      // 조건이 해당하면 종료
+      return;
+    }
+
+    // 토큰에서 획득
+    String username = jwtUtil.getUsername(token);
+    String role = jwtUtil.getRole(token);
+
+    UserEntity user = new UserEntity();
+    user.setUsername(username);
+    user.setPassword("PASSword1234");
+    user.setRole(role);
+
+    CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+    // 인증 토큰 생성
+    Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+    // 세션에 사용자 등록
+    SecurityContextHolder.getContext().setAuthentication(authToken);
+
+    filterChain.doFilter(request, response);
+
+
+  }
+}
---
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/auth/security/SecurityConfiguration.java
@@ -0,0 +1,74 @@
+package org.zerobase.hospitalappointmentproject.global.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+  private final AuthenticationConfiguration authenticationConfiguration;
+  private final JwtUtil jwtUtil;
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+    return configuration.getAuthenticationManager();
+
+  }
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+    return new BCryptPasswordEncoder();
+
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    // csrf disable
+    http.csrf(AbstractHttpConfigurer::disable);
+
+    // From 로그인 방식 disable
+    http.formLogin(AbstractHttpConfigurer::disable);
+
+    // http basic 인증 방식 disable
+    http.httpBasic(AbstractHttpConfigurer::disable);
+
+    // 경로별 인가 작업
+    http.authorizeHttpRequests((auth) -> auth
+        .requestMatchers("/**").permitAll()
+//        .requestMatchers("").hasRole("PATIENT")
+//        .requestMatchers("").hasRole("DOCTOR")
+//        .requestMatchers("").hasRole("STAFF")
+        .anyRequest().authenticated());
+
+    // jwt 필터 추가
+    http.addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+    // login 필터 추가
+    http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+                                      UsernamePasswordAuthenticationFilter.class);
+
+    // 세션 설정
+    http.sessionManagement((session) -> session
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return http.build();
+
+  }
+
+}
