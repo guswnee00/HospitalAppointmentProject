a291817442db881e26c4942ce57b0666e5c04ebd
Hyunju Lee <guswnee00@gmail.com>
2024-05-28 21:27:04 +0900

    feat : hospital controller for register

---
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/hospital/controller/HospitalController.java
@@ -0,0 +1,29 @@
+package org.zerobase.hospitalappointmentproject.domain.hospital.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerobase.hospitalappointmentproject.domain.hospital.dto.HospitalDto;
+import org.zerobase.hospitalappointmentproject.domain.hospital.dto.HospitalRegister;
+import org.zerobase.hospitalappointmentproject.domain.hospital.service.HospitalService;
+
+@RestController
+@RequiredArgsConstructor
+public class HospitalController {
+
+  private final HospitalService hospitalService;
+
+  @PostMapping("/staff/my-hospital")
+  public ResponseEntity<?> register(@AuthenticationPrincipal UserDetails userDetails,
+                                    @RequestBody HospitalRegister.Request request
+  ) {
+    String username = userDetails.getUsername();
+    HospitalDto hospitalDto = hospitalService.register(username, request);
+    return ResponseEntity.ok(HospitalRegister.Response.fromDto(hospitalDto));
+  }
+
+}
