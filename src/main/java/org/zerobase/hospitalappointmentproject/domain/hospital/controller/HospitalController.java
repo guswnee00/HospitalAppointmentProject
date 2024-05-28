347a7016c0283bc313a5e698ad195dc176fe8364
Hyunju Lee <guswnee00@gmail.com>
2024-05-29 03:49:16 +0900

    feat : hospital controller for getting info

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/hospital/controller/HospitalController.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/hospital/controller/HospitalController.java
@@ -5,11 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerobase.hospitalappointmentproject.domain.hospital.dto.HospitalDto;
+import org.zerobase.hospitalappointmentproject.domain.hospital.dto.HospitalInfoResponse;
 import org.zerobase.hospitalappointmentproject.domain.hospital.dto.HospitalInfoUpdate;
 import org.zerobase.hospitalappointmentproject.domain.hospital.dto.HospitalRegister;
 import org.zerobase.hospitalappointmentproject.domain.hospital.service.HospitalService;
@@ -46,6 +49,12 @@ public class HospitalController {
     String username = userDetails.getUsername();
     hospitalService.delete(username, request.getPassword());
     return ResponseEntity.ok("병원 삭제가 완료되었습니다.");
+  }
+
+  @GetMapping("/{hospitalName}")
+  public ResponseEntity<?> getInfo(@PathVariable String hospitalName) {
+    HospitalDto hospitalDto = hospitalService.getInfo(hospitalName);
+    return ResponseEntity.ok(HospitalInfoResponse.fromDto(hospitalDto));
   }
 
 }
