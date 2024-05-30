04d7cd534528c118123ce60d8a12950be5f139cf
Hyunju Lee <guswnee00@gmail.com>
2024-05-31 03:03:18 +0900

    feat : find doctor available time

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/repository/AppointmentRepository.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/repository/AppointmentRepository.java
@@ -1,12 +1,15 @@
 package org.zerobase.hospitalappointmentproject.domain.appointment.repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zerobase.hospitalappointmentproject.domain.appointment.entity.AppointmentEntity;
+import org.zerobase.hospitalappointmentproject.domain.doctor.entity.DoctorEntity;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentEntity, Long> {
 
-
+  boolean existsByDoctorAndAppointmentDateAndAppointmentTime(DoctorEntity doctor, LocalDate date, LocalTime time);
 
 }
