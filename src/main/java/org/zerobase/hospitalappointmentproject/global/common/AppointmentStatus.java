125c475b9401d571dfd02a3a98de1dfdf82b6383
Hyunju Lee <guswnee00@gmail.com>
2024-06-01 19:37:39 +0900

    refactor : rename

--- src/main/java/org/zerobase/hospitalappointmentproject/global/common/AppointmentStatus.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/common/AppointmentStatus.java
@@ -4,7 +4,7 @@ public enum AppointmentStatus {
 
   PENDING_APPOINTMENT,      // 예약 대기
   CONFIRMED_APPOINTMENT,    // 예약 확정
-  CANCELED_APPOINTMENT,     // 예약 취소
+  NO_SHOW,                  // 노쇼
   WAITING_CONSULTATION,     // 진료 대기
   COMPLETE_CONSULTATION     // 진료 완료
 
