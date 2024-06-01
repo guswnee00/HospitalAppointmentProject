2bd76c1cd9ce83528e550a60b4095d23ee93d7ea
Hyunju Lee <guswnee00@gmail.com>
2024-06-01 19:34:42 +0900

    feat : appointment controller for cancel

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/controller/AppointmentController.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/controller/AppointmentController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,6 +38,15 @@ public class AppointmentController {
     String username = userDetails.getUsername();
     AppointmentDto appointmentDto = appointmentService.update(request, appointmentId, username);
     return ResponseEntity.ok(AppointmentUpdate.Response.fromDto(appointmentDto));
+  }
+
+  @DeleteMapping("/patient/my-appointment/{appointmentId}")
+  public ResponseEntity<?> cancel(@AuthenticationPrincipal UserDetails userDetails,
+                                  @PathVariable Long appointmentId
+  ) {
+    String username = userDetails.getUsername();
+    appointmentService.cancel(username, appointmentId);
+    return ResponseEntity.ok("예약 취소가 완료되었습니다.");
   }
 
 }
