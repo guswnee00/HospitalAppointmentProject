668e78d294eb439f389e4e35fd3ee91cf9780a6a
Hyunju Lee <guswnee00@gmail.com>
2024-06-01 22:55:49 +0900

    feat : add controller pagination for patient appointment

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/controller/AppointmentController.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/controller/AppointmentController.java
@@ -1,14 +1,21 @@
 package org.zerobase.hospitalappointmentproject.domain.appointment.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerobase.hospitalappointmentproject.domain.appointment.dto.AppointmentCreate;
 import org.zerobase.hospitalappointmentproject.domain.appointment.dto.AppointmentDto;
@@ -47,6 +54,19 @@ public class AppointmentController {
     String username = userDetails.getUsername();
     appointmentService.cancel(username, appointmentId);
     return ResponseEntity.ok("예약 취소가 완료되었습니다.");
+  }
+
+  @GetMapping("/patient/my-appointment")
+  public ResponseEntity<?> patientAppointments(@AuthenticationPrincipal UserDetails userDetails,
+                                               @RequestParam(required = false, defaultValue = "appointmentDate") String sortBy,
+                                               @RequestParam(required = false, defaultValue = "asc") String sortDirection,
+                                               @PageableDefault Pageable pageable
+  ) {
+    String username = userDetails.getUsername();
+    Sort.Direction direction = Sort.Direction.fromString(sortDirection);
+    Pageable sortPage = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(direction, sortBy));
+    Page<AppointmentDto> appointments = appointmentService.patientAppointments(username, sortPage);
+    return ResponseEntity.ok(appointments);
   }
 
 }
