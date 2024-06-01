70e3c7b3b51cca6bcb88a69b985ae0aa1ee2e0b3
Hyunju Lee <guswnee00@gmail.com>
2024-06-01 22:55:16 +0900

    feat : add repo method pagination for patient appointment

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/repository/AppointmentRepository.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/appointment/repository/AppointmentRepository.java
@@ -3,15 +3,19 @@ package org.zerobase.hospitalappointmentproject.domain.appointment.repository;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zerobase.hospitalappointmentproject.domain.appointment.entity.AppointmentEntity;
 import org.zerobase.hospitalappointmentproject.domain.doctor.entity.DoctorEntity;
+import org.zerobase.hospitalappointmentproject.domain.patient.entity.PatientEntity;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentEntity, Long> {
 
   boolean existsByDoctorAndAppointmentDateAndAppointmentTime(DoctorEntity doctor, LocalDate date, LocalTime time);
   Optional<AppointmentEntity> findByIdAndPatient_Username(Long id, String username);
+  Page<AppointmentEntity> findByPatient(PatientEntity patient, Pageable pageable);
 
 }
