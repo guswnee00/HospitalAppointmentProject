9bcafa4ee25a30973fc63ebcef8564c6923657a2
Hyunju Lee <guswnee00@gmail.com>
2024-06-04 00:37:56 +0900

    refactor : remove map struct and add conversion method

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/patient/dto/PatientDto.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/patient/dto/PatientDto.java
@@ -2,14 +2,18 @@ package org.zerobase.hospitalappointmentproject.domain.patient.dto;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.zerobase.hospitalappointmentproject.domain.appointment.dto.AppointmentDto;
-import org.zerobase.hospitalappointmentproject.domain.medicalrecord.dto.MedicalRecordDto;
+import org.zerobase.hospitalappointmentproject.domain.appointment.entity.AppointmentEntity;
+import org.zerobase.hospitalappointmentproject.domain.medicalrecord.entity.MedicalRecordEntity;
+import org.zerobase.hospitalappointmentproject.domain.patient.entity.PatientEntity;
 import org.zerobase.hospitalappointmentproject.global.common.GenderType;
 
 @Setter
@@ -30,10 +34,38 @@ public class PatientDto {
   private LocalDate birthDate;
   private String address;
 
-  private Set<MedicalRecordDto> medicalRecords;
-  private Set<AppointmentDto> appointments;
+  private Set<Long> medicalRecords;
+  private Set<Long> appointments;
 
   private LocalDateTime createdAt;
   private LocalDateTime modifiedAt;
+
+  public static PatientDto toDto(PatientEntity entity) {
+
+    return PatientDto.builder()
+        .username(entity.getUsername())
+        .password(entity.getPassword())
+        .role(entity.getRole())
+        .name(entity.getName())
+        .phoneNumber(entity.getPhoneNumber())
+        .email(entity.getEmail())
+        .gender(entity.getGender())
+        .birthDate(entity.getBirthDate())
+        .address(entity.getAddress())
+        .medicalRecords(Optional.ofNullable(entity.getMedicalRecords())
+                                .orElse(Collections.emptySet())
+                                .stream()
+                                .map(MedicalRecordEntity::getId)
+                                .collect(Collectors.toSet()))
+        .appointments(Optional.ofNullable(entity.getAppointments())
+                              .orElse(Collections.emptySet())
+                              .stream()
+                              .map(AppointmentEntity::getId)
+                              .collect(Collectors.toSet()))
+        .createdAt(entity.getCreatedAt())
+        .modifiedAt(entity.getModifiedAt())
+        .build();
+
+  }
 
 }
