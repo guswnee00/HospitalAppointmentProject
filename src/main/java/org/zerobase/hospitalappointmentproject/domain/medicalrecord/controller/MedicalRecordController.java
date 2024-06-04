be199638835c57d83a04471576eb638b032b4b49
Hyunju Lee <guswnee00@gmail.com>
2024-06-04 15:54:18 +0900

    feat : controller for creating medical record

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/medicalrecord/controller/MedicalRecordController.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/medicalrecord/controller/MedicalRecordController.java
@@ -1,12 +1,29 @@
 package org.zerobase.hospitalappointmentproject.domain.medicalrecord.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerobase.hospitalappointmentproject.domain.medicalrecord.dto.MedicalRecordCreate;
+import org.zerobase.hospitalappointmentproject.domain.medicalrecord.dto.MedicalRecordDto;
+import org.zerobase.hospitalappointmentproject.domain.medicalrecord.service.MedicalRecordService;
 
 @RestController
 @RequiredArgsConstructor
 public class MedicalRecordController {
 
+  private final MedicalRecordService medicalRecordService;
 
+  @PostMapping("/doctor/medical-record")
+  public ResponseEntity<?> create(@AuthenticationPrincipal UserDetails userDetails,
+                                  @RequestBody MedicalRecordCreate.Request request
+  ) {
+    String username = userDetails.getUsername();
+    MedicalRecordDto medicalRecordDto = medicalRecordService.create(request, username);
+    return ResponseEntity.ok(MedicalRecordCreate.Response.fromDto(medicalRecordDto));
+  }
 
 }
