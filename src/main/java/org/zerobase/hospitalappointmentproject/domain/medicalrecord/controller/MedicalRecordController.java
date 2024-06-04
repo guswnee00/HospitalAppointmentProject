b5decad92ec5d356592e95abe69b541f6f5b70e9
Hyunju Lee <guswnee00@gmail.com>
2024-06-04 16:49:15 +0900

    feat : controller for updating medical record

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/medicalrecord/controller/MedicalRecordController.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/medicalrecord/controller/MedicalRecordController.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerobase.hospitalappointmentproject.domain.medicalrecord.dto.MedicalRecordCreate;
 import org.zerobase.hospitalappointmentproject.domain.medicalrecord.dto.MedicalRecordDto;
+import org.zerobase.hospitalappointmentproject.domain.medicalrecord.dto.MedicalRecordUpdate;
 import org.zerobase.hospitalappointmentproject.domain.medicalrecord.service.MedicalRecordService;
 
 @RestController
@@ -24,6 +27,16 @@ public class MedicalRecordController {
     String username = userDetails.getUsername();
     MedicalRecordDto medicalRecordDto = medicalRecordService.create(request, username);
     return ResponseEntity.ok(MedicalRecordCreate.Response.fromDto(medicalRecordDto));
+  }
+
+  @PatchMapping("/doctor/medical-record/{medicalRecordId}")
+  public ResponseEntity<?> update(@AuthenticationPrincipal UserDetails userDetails,
+                                  @PathVariable Long medicalRecordId,
+                                  @RequestBody MedicalRecordUpdate.Request request
+  ) {
+    String username = userDetails.getUsername();
+    MedicalRecordDto medicalRecordDto = medicalRecordService.update(request, username, medicalRecordId);
+    return ResponseEntity.ok(MedicalRecordUpdate.Response.fromDto(medicalRecordDto));
   }
 
 }
