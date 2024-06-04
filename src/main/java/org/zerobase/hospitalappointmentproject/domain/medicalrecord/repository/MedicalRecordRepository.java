dde29bb04413977b1004bb843cd4919f32cc452f
Hyunju Lee <guswnee00@gmail.com>
2024-06-04 18:22:18 +0900

    feat : add method

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/medicalrecord/repository/MedicalRecordRepository.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/medicalrecord/repository/MedicalRecordRepository.java
@@ -1,6 +1,8 @@
 package org.zerobase.hospitalappointmentproject.domain.medicalrecord.repository;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.zerobase.hospitalappointmentproject.domain.doctor.entity.DoctorEntity;
@@ -10,4 +12,6 @@ import org.zerobase.hospitalappointmentproject.domain.medicalrecord.entity.Medic
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecordEntity, Long> {
 
   Optional<MedicalRecordEntity> findByIdAndDoctor(Long id, DoctorEntity doctor);
+  Page<MedicalRecordEntity> findAllByDoctor(DoctorEntity doctor, Pageable pageable);
+
 }
