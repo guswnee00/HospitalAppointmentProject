ed3382b914a86e27b27b9171e9c682a8f30c0084
Hyunju Lee <guswnee00@gmail.com>
2024-06-08 23:37:22 +0900

    feat : update doctor document for es

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/doctor/document/DoctorDocument.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/doctor/document/DoctorDocument.java
@@ -1,5 +1,7 @@
 package org.zerobase.hospitalappointmentproject.domain.doctor.document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -27,6 +30,24 @@ public class DoctorDocument {
   private Long id;
 
   @Field(type = FieldType.Text)
+  private String username;
+
+  @Field(type = FieldType.Text)
+  private String password;
+
+  @Field(type = FieldType.Text)
+  private String role;
+
+  @Field(type = FieldType.Text)
+  private String name;
+
+  @Field(type = FieldType.Text)
+  private String phoneNumber;
+
+  @Field(type = FieldType.Text)
+  private String email;
+
+  @Field(type = FieldType.Text)
   private String bio;
 
   @Field(type = FieldType.Nested)
@@ -40,5 +61,13 @@ public class DoctorDocument {
 
   @Field(type = FieldType.Nested)
   private Set<AppointmentDocument> appointments;
+
+  @Field(type = FieldType.Date, format = DateFormat.date_time)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  private LocalDateTime createdAt;
+
+  @Field(type = FieldType.Date, format = DateFormat.date_time)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  private LocalDateTime modifiedAt;
 
 }
