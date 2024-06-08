a870f963db404b6780ca4498df178a5004edfe8f
Hyunju Lee <guswnee00@gmail.com>
2024-06-09 00:01:53 +0900

    feat : init synchronization service

---
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/sync/DataSyncService.java
@@ -0,0 +1,12 @@
+package org.zerobase.hospitalappointmentproject.global.sync;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DataSyncService {
+
+
+
+}
