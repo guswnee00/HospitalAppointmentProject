32d3264ec770c261bef4a0abdee2a4148222980a
Hyunju Lee <guswnee00@gmail.com>
2024-06-10 03:45:40 +0900

    feat : synchronization schedule

---
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/sync/DataSyncScheduler.java
@@ -0,0 +1,18 @@
+package org.zerobase.hospitalappointmentproject.global.sync;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataSyncScheduler {
+
+  private final DataSyncService dataSyncService;
+
+  @Scheduled(cron = "0 0 * * * ?")
+  public void scheduleForSynchronization() {
+    dataSyncService.syncAll();
+  }
+
+}
