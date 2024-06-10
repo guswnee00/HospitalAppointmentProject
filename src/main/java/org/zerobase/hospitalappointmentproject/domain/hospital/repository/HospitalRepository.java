612bebae83455d369743f12364bf3a9ccb8f5427
Hyunju Lee <guswnee00@gmail.com>
2024-06-11 00:58:03 +0900

    refactor : remove unused repo methods

--- src/main/java/org/zerobase/hospitalappointmentproject/domain/hospital/repository/HospitalRepository.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/domain/hospital/repository/HospitalRepository.java
@@ -1,10 +1,7 @@
 package org.zerobase.hospitalappointmentproject.domain.hospital.repository;
 
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.zerobase.hospitalappointmentproject.domain.hospital.entity.HospitalEntity;
 
@@ -13,13 +10,5 @@ public interface HospitalRepository extends JpaRepository<HospitalEntity, Long> 
 
   Optional<HospitalEntity> findByName(String name);
   boolean existsByName(String name);
-  List<HospitalEntity> findDistinctByDoctors_Specialty_Name(String specialtyName);
-  @Query(value = "SELECT h.*, " +
-      "(6371 * acos(cos(radians(:lat)) * cos(radians(h.latitude)) * cos(radians(h.longitude) - radians(:lon)) + sin(radians(:lat)) * sin(radians(h.latitude)))) AS distance " +
-      "FROM Hospital h " +
-      "WHERE (6371 * acos(cos(radians(:lat)) * cos(radians(h.latitude)) * cos(radians(h.longitude) - radians(:lon)) + sin(radians(:lat)) * sin(radians(h.latitude)))) < :radius " +
-      "ORDER BY distance",
-      nativeQuery = true)
-  List<HospitalEntity> findNearByHospital(@Param("lat") double lat, @Param("lon") double lon, @Param("radius") double radius);
 
 }
