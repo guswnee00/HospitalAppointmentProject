8d393857920986faa3e78dcdcef08409b8ff301c
Hyunju Lee <guswnee00@gmail.com>
2024-06-11 23:00:27 +0900

    feat : sms service for appointment

--- src/main/java/org/zerobase/hospitalappointmentproject/global/sms/SmsService.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/sms/SmsService.java
@@ -1,19 +1,48 @@
 package org.zerobase.hospitalappointmentproject.global.sms;
 
+import static org.zerobase.hospitalappointmentproject.global.exception.ErrorCode.FAILED_TO_SEND_MESSAGE;
+
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.zerobase.hospitalappointmentproject.global.exception.CustomException;
 
 @Service
 public class SmsService {
 
   private final DefaultMessageService messageService;
 
-  public SmsService(@Value("${sprint.coolsms.api.key}") String apiKey,
-                    @Value("${sprint.coolsms.api.secret}") String apiSecret,
-                    @Value("${sprint.coolsms.api.domain}") String apiDomain
-  ) {
-    this.messageService = new DefaultMessageService(apiKey, apiSecret, apiDomain);
+  @Value("${sprint.coolsms.api.key}")
+  private String apiKey;
+
+  @Value("${sprint.coolsms.api.secret}")
+  private String apiSecret;
+
+  @Value("${sprint.coolsms.api.domain}")
+  private String apiDomain;
+
+  public SmsService() {
+    this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, apiDomain);
+  }
+
+  public void sendSms(String to, String from, String text) throws CustomException {
+
+    Message message = new Message();
+    message.setFrom(from);
+    message.setTo(to);
+    message.setText(text);
+
+    SingleMessageSendingRequest request = new SingleMessageSendingRequest(message);
+    SingleMessageSentResponse response = this.messageService.sendOne(request);
+
+    if (response != null && !response.getStatusCode().equals("200")) {
+      throw new CustomException(FAILED_TO_SEND_MESSAGE);
+    }
+
   }
 
 }
