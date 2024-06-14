b6237b749120ab6ea1322134911890e31ee94033
Hyunju Lee <guswnee00@gmail.com>
2024-06-14 21:33:59 +0900

    feat : init redis config

---
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/config/RedisConfiguration.java
@@ -0,0 +1,8 @@
+package org.zerobase.hospitalappointmentproject.global.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfiguration {
+
+}
