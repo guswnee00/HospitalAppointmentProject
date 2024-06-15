3c63113e6d2a95e604954cc6b66e31573f0a4dab
Hyunju Lee <guswnee00@gmail.com>
2024-06-15 17:26:21 +0900

    refactor : change file path

--- src/main/java/org/zerobase/hospitalappointmentproject/global/config/EnvConfiguration.java
+++ src/main/java/org/zerobase/hospitalappointmentproject/global/config/EnvConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("file:${PWD}/.env")
+@PropertySource("classpath:/env.properties")
 public class EnvConfiguration {
 
 }
