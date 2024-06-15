29fbd9008d02d1cefaf8d5f24440ccd274384ff8
Hyunju Lee <guswnee00@gmail.com>
2024-06-15 17:26:05 +0900

    refactor : delete config

--- src/main/java/org/zerobase/hospitalappointmentproject/global/config/RedisConfiguration.java
+++
@@ -1,8 +0,0 @@
-package org.zerobase.hospitalappointmentproject.global.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RedisConfiguration {
-
-}
